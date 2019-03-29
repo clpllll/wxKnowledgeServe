3e672639aa97c98f2c62b88d3d175d9d29813da3
clp <clp@qq.com>
2019-03-29 16:19:57 +0800

    node

--- serve.js
+++ serve.js
@@ -1,5 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017";
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  dbo = db.db("wx");
+  app.context.dbo = dbo;
+});
 app.use(async ctx => {
   ctx.body = "hello world";
 })
